5768e60e6f5726fd1a14eef7a45600b8850814f5
Dhrubojyoti Biswas <46414440+DhrubojyotiBis1@users.noreply.github.com>
2024-09-18 22:32:32 +0530

    Added Api To Fetch All Registered Gamers (#3)

--- gamers/views.py
+++ gamers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Gamers
+
+#Global Constants
+SUCESS = 'Sucess'
+
+#API Functions
+@api_view(['GET'])
+def getAll(request):
+    ''' API to get all gamers '''
+    
+    gamers = Gamers.objects.all().values()
+    return Response({'message': SUCESS, 'gamers': gamers}, 200)
